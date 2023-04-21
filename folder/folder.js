f244ecf4e9e32ba6bec7137f2004bdad43173fe2
kristian1402 <ksaren20@student.aau.dk>
2023-04-21 12:15:46 +0200

    kigger på live search

--- folder/folder.js
+++ folder/folder.js
@@ -10,7 +10,7 @@ function loadDoc() {
   xhttp.send();
 }
 
-function loadMain() {
+function loadMain(url) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -18,6 +18,6 @@ function loadMain() {
       this.responseText;
     }
   };
-  xhttp.open("GET", "main.txt", true);
+  xhttp.open("GET", url, true);
   xhttp.send();
 }
