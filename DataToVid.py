24a5b3d8cf1fd34d35c96e1534622ceb79939a59
Hatzi2 <72734884+Hatzi2@users.noreply.github.com>
2023-04-24 18:15:45 +0200

    Python = done

--- DataToVid.py
+++ DataToVid.py
@@ -3,6 +3,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import cv2
+
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -13,24 +15,41 @@ file_list = os.listdir(folder_path)
 for file_name in file_list:
     os.remove(os.path.join(folder_path, file_name))
 
-class ImageMaker():
 
+class Scrubber():
+    def __init__(self, filename):
+        df = pd.read_csv(filename, delimiter=";", decimal=",")  # Read the data from csv file
+        Px = df['X'].round(1)  # Round X to 1 decimal place
+        Py = df['Z'].round(1)  # Round Z to 1 decimal place
+        Positions = pd.concat([Px, Py], axis=1)  # Combine Px and Py into a DataFrame
+        unique_rows = Positions.drop_duplicates()  # Remove duplicate rows
+        counts = Positions.groupby(list(unique_rows.columns)).size().reset_index(name='Count')  # Count the number of occurrences for each unique row
+        SaveFile = pd.merge(unique_rows, counts, on=list(unique_rows.columns))  # Merge unique rows with their corresponding count values
+        header = "X;Z;Count"
+        SaveFile.to_csv('TesterCleaned.csv', sep=';', header=header, index=False, float_format='%.1f')  # Save the DataFrame to a CSV file
+        print(SaveFile)
+
+
+Scrubber = Scrubber('tester.csv')
+
+
+def ImageMaker():
     i = 1
 
     df = pd.read_csv('TesterCleaned.csv', sep=";", header=0)
 
-    # Get the min and max values for X and Z from the entire dataset
+   # Get the min and max values for X and Z from the entire dataset
     x_min = df['X'].min()
     x_max = df['X'].max()
     z_min = df['Z'].min()
     z_max = df['Z'].max()
+    num_rows, num_cols = df.shape
 
     # Define the size of the figure in inches
     fig_size = (8, 6)
 
-    for f in range(10, len(df)+1, 10):
+    for f in range(10, len(df), 10):
         print(f)
-
         # Slice the dataframe up until the current iteration
         df_slice = df.iloc[:f, :]
 
@@ -44,12 +63,6 @@ class ImageMaker():
         fig, ax = plt.subplots(figsize=fig_size)
         sns.heatmap(pivot, annot=False, cmap='YlOrRd', cbar=False, vmin=0, vmax=max_value, ax=ax)
 
-        # Add points to the corners of the plot
-        ax.plot([x_min, x_min], [z_min, z_min], marker='o', color='black')
-        ax.plot([x_max, x_max], [z_min, z_min], marker='o', color='black')
-        ax.plot([x_min, x_min], [z_max, z_max], marker='o', color='black')
-        ax.plot([x_max, x_max], [z_max, z_max], marker='o', color='black')
-
         # Save the plot to a file in the tmpFiles directory
         save_at = os.path.join(script_dir, 'tmpFiles', str(i) + '.png')
         plt.savefig(save_at, dpi=300, bbox_inches='tight')
@@ -59,3 +72,37 @@ class ImageMaker():
 
         # Increment the counter
         i += 1
+ImageMaker()
+def VideoMaker():
+
+    # Set the name and fps of the output video
+    video_name = 'output.mp4'
+    fps = 5
+
+    # Get a list of all the image filenames in the folder
+    images = [img for img in os.listdir(folder_path) if img.endswith('.png')]
+
+    # Sort the image filenames in ascending order
+    images.sort(key=lambda x: int(x[:-4]))
+
+    # Get the height and width of the first image in the folder
+    frame = cv2.imread(os.path.join(folder_path, images[0]))
+    height, width, layers = frame.shape
+
+    # Create a VideoWriter object to write the frames to a video
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video = cv2.VideoWriter(video_name, fourcc, fps, (width, height))
+
+    # Iterate over each image and add it to the video
+    for image in images:
+        # Read the image from the file
+        frame = cv2.imread(os.path.join(folder_path, image))
+        frame = cv2.resize(frame, (width, height))
+        video.write(frame)
+
+    # Release the VideoWriter object
+    video.release()
+    for image in images:
+        os.remove(os.path.join(folder_path, image))
+
+VideoMaker()
