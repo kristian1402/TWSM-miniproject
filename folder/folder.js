9a6fbd2644aa2736c53a75fda8f3c824f6e97829
kristian1402 <ksaren20@student.aau.dk>
2023-04-24 13:45:42 +0200

    disse nødder

--- folder/folder.js
+++ folder/folder.js
@@ -35,6 +35,6 @@ function showResult(str) {
       document.getElementById("livesearch").style.border="1px solid #A5ACB2";
     }
   }
-  xmlhttp.open("GET","livesearch.php?q="+str,true);
+  xmlhttp.open("GET",str,true);
   xmlhttp.send();
 }
