6114826ce13cfe2fbc08a3a92d3a2407e5ec763c
Hatzi2 <72734884+Hatzi2@users.noreply.github.com>
2023-04-25 18:19:35 +0200

    comments

--- html/databaseConnect.php
+++ html/databaseConnect.php
@@ -6,28 +6,28 @@ $dbname = "csvfiles";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
+// Check if there is any connection
 if (mysqli_connect_error()) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Define column names
+// Create an array with collum names: COL 1-4 this is hardcoded which is very nice :)
 $headers = array("COL 1", "COL 2", "COL 3", "COL 4");
 
-// Set character set and delimiter for CSV file
+// Set character set and allow php to automatically detect line changes
 mysqli_set_charset($conn, "utf8");
 ini_set('auto_detect_line_endings', true);
 
-// Perform a SQL query
+// The from should be a string which changes based on which dataset wants to be downloaded. having tester here makes it hard coded
 $sql = "SELECT `" . implode("`, `", $headers) . "` FROM tester";
 $result = mysqli_query($conn, $sql);
 
-// Check if the query was successful
+// make sure query was sucessfull
 if (!$result) {
     die("Error: " . mysqli_error($conn));
 }
 
-// Set the header information
+// Set filename and data header
 header('Content-Type: text/csv; charset=utf-8');
 header('Content-Disposition: attachment; filename=data.csv');
 
