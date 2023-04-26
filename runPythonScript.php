712ffc4b5e0a89c2c7924eed4cf3b7211e29c921
kristian1402 <ksaren20@student.aau.dk>
2023-04-26 14:06:22 +0200

    update with AJAX

--- runPythonScript.php
+++ runPythonScript.php
@@ -1,10 +1,12 @@
 <?php
-$pythonPath = "C:\Users\madsw\AppData\Local\Programs\Python\Python39\python.exe"; // Replace with the path to your Python interpreter
+$pythonPath = "C:\Users\khs14\AppData\Local\Programs\Python\Python311\python.exe"; // Replace with the path to your Python interpreter
 $scriptPath = __DIR__ . '\DataToVid.py'; // Replace with the path to your Python script
 $existingXMLPath = __DIR__ . '\folder\links.xml'; // Path to existing XML file
 
-// Extract the title element and convert it to a string
-$title = (string) simplexml_load_file($existingXMLPath)->link[0]->title;
+$myVariable = $_GET["myVariable"];
+$toInt = (int)$myVariable;
+$title = (string) simplexml_load_file($existingXMLPath)->link[$toInt-1]->title; // Extract the title element and convert it to a string
+
 
 // Construct the command to run the Python script with the title as an argument
 $command = "{$pythonPath} {$scriptPath} {$title}";
@@ -13,5 +15,5 @@ $command = "{$pythonPath} {$scriptPath} {$title}";
 $output = shell_exec($command);
 
 // Output the result for debugging purposes
-echo "Done";
+echo $title;
 ?>
